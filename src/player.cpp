d1a8d26bc938afd0632d9be81c7ec3b42f04b29b
Johncore <32520764+Johncorex@users.noreply.github.com>
2021-02-20 00:36:00 -0300

    Fix: Logout when player is training exercise items @henriquesameshima
    Fix: Logout when player is training exercise items
    
    Co-authored-by: henriquesameshima <sameshima@live.com>

--- src/player.cpp
+++ src/player.cpp
@@ -483,6 +483,7 @@ void Player::setTraining(bool value) {
 			it.second->notifyStatusChange(this, value ? VIPSTATUS_TRAINING : VIPSTATUS_ONLINE, false);
 		}
 	}
+	setExerciseTraining(value);
 }
 
 void Player::addSkillAdvance(skills_t skill, uint64_t count)
@@ -1778,7 +1779,7 @@ void Player::onThink(uint32_t interval)
 		addMessageBuffer();
 	}
 
-	if (!getTile()->hasFlag(TILESTATE_NOLOGOUT) && !isAccessPlayer()) {
+	if (!getTile()->hasFlag(TILESTATE_NOLOGOUT) && !isAccessPlayer() && !isExerciseTraining()) {
 		idleTime += interval;
 		const int32_t kickAfterMinutes = g_config.getNumber(ConfigManager::KICK_AFTER_MINUTES);
 		if (idleTime > (kickAfterMinutes * 60000) + 60000) {
